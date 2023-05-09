06014d487654c923df8caabd7a987c01c7daecdf
Ne'evaiti <52328785+neevaiti@users.noreply.github.com>
2023-05-09 12:07:24 +0200

    feat: cleaning the dataset

---
+++ todo.py
@@ -0,0 +1,12 @@
+"""
+TODO list (Partie nettoyage) :
+- Suppression des doublons
+- Conversion des types
+- Traitement des valeurs manquantes
+- Consistance des données (valeurs aberrantes, fautes orthographes…)
+
+
+
+TODO list (Partie analyse) :
+
+"""
